79e190ff37ee8c3cc2333445ad44f20f54c4acaf
Michael Wentz <31914755+mwentzWW@users.noreply.github.com>
2024-12-01 05:27:57 +0000

    made multipage app

---
+++ Home.py
@@ -0,0 +1,60 @@
+import streamlit as st
+import streamlit.components.v1 as components
+
+import utils
+
+st.set_page_config(
+    page_title="Home",
+    page_icon="🧙‍♂️",
+    layout='wide'
+)
+
+st.sidebar.success("Select a page above to get started.")
+
+st.write("# Welcome to my app 🧙‍♂️")
+
+st.write_stream(utils.home_message())
+
+st.markdown(
+    """
+
+    ## App Introduction
+
+    [Streamlit](https://streamlit.io) is an open-source app framework built specifically for
+    Machine Learning and Data Science projects.
+
+    This is my first app built using streamlit.
+
+    My goal for this project is to learn the basics of streamlit apps and deployment. I'll create a simple historical value at risk (VaR) tool for treasury bond analysis based on public data.
+
+    ## VaR Methodology (Fixed Income Assets)
+
+    Model workflow:
+
+        1. We will generate the absolute and relative changes in treasury par rates in the dataset.
+
+        2. The user will submit information about their instrument and it will be mapped to a treasury benchmark.
+
+        3. Based on the mapping and size of the position, the VaR measure will be estimated.
+
+    $$\mathbb{N} = \{ a \in \mathbb{Z} : a > 0 \}$$
+
+    ## VaR Resources
+
+    * [2010 IMF Conference On Operationalizing Systemic Risk Monitoring](https://www.imf.org/external/np/seminars/eng/2010/mcm/pdf/RBerner.pdf)
+    * Ken Abbott's MIT Lecture on Value at Risk (VAR) Models 
+        * [Video Link](https://ocw.mit.edu/courses/18-s096-topics-in-mathematics-with-applications-in-finance-fall-2013/resources/lecture-7-value-at-risk-var-models/)
+        * [Lecture Notes](https://ocw.mit.edu/courses/18-s096-topics-in-mathematics-with-applications-in-finance-fall-2013/resources/mit18_s096f13_lecnote7/)
+    * [CME Calculate Dollar Value of Basis Point (DVBP or DV01)](https://www.cmegroup.com/trading/interest-rates/files/Calculating_the_Dollar_Value_of_a_Basis_Point.pdf)
+    * [CFI Duration](https://corporatefinanceinstitute.com/resources/fixed-income/duration/)
+
+    
+    ## Data Source
+
+    [Treasury Interest Rates Par Yield Curve](https://home.treasury.gov/interest-rates-data-csv-archive)
+
+
+"""
+)
+
+
---
+++ pages/1_🧪 Treasury Data.py
@@ -0,0 +1,103 @@
+import streamlit as st
+
+import utils
+import charts
+
+st.set_page_config(
+    page_title="Treasury Data",
+    page_icon="🧪",
+    layout='wide'
+)
+
+st.title("🧪 Treasury Data")
+
+df_hist = utils.create_dataframe()
+
+st.session_state['df_hist'] = df_hist
+
+df_abs_diff = utils.generate_abs_diffs(df_hist)
+
+st.session_state['df_abs_diff'] = df_abs_diff
+
+df_rel_diff = utils.generate_rel_diffs(df_hist)
+
+st.session_state['df_rel_diff'] = df_rel_diff
+
+st.write_stream(utils.data_message)
+
+data_col, chart_col = st.columns(2)
+
+with open('utils.py', 'r') as f:
+
+    code: list = f.readlines()
+
+with data_col:
+
+    st.write("## Historical Treasury Data")
+
+    st.dataframe(df_hist)
+
+    st.write("## Absolute 1-Day Rate Changes in Basis Points (bp)")
+
+    st.markdown("**Python Source Code**")
+
+    st.code(" ".join(code[33:48]), language='python')
+
+    st.dataframe(df_abs_diff)
+
+    st.write("### Stats")
+
+    st.dataframe(df_abs_diff.describe(percentiles=[0.01, 0.05, 0.10, 0.25, 0.50, 0.75, 0.90, 0.95, 0.99]))
+
+    st.divider()
+
+    st.write("## Relative 1-Day Rate Changes in decimal")
+
+    st.markdown("**Python Source Code**")
+
+    st.code(" ".join(code[61:73]), language='python')
+
+    st.dataframe(df_rel_diff)
+
+    st.write("### Stats")
+
+    st.dataframe(df_rel_diff.describe(percentiles=[0.01, 0.05, 0.10, 0.25, 0.50, 0.75, 0.90, 0.95, 0.99]))
+
+with chart_col:
+
+    #st.plotly_chart(charts.rates_curve_chart(df=df_hist), use_container_width=True)
+
+    with st.form("treasury_selection"):
+
+        option = st.selectbox(
+            "Which Treasury do you want to view?",
+            st.session_state.df_hist.columns,
+            index=None,
+            placeholder="Select a Treasury...",
+            )
+        
+        rel_toggle = st.toggle("Switch to relative charts (Defaults to absolute changes)")
+
+        submitted = st.form_submit_button("Submit")
+
+    if submitted:
+
+        st.write("You selected:", option)
+
+        if rel_toggle:
+
+            df_diff = df_rel_diff
+
+        else:
+
+            df_diff = df_abs_diff
+
+        st.dataframe(df_diff[option].describe(percentiles=[0.01, 0.05, 0.10, 0.25, 0.50, 0.75, 0.90, 0.95, 0.99]).transpose())
+
+        st.plotly_chart(charts.rate_line_chart(df=df_hist, y=option), use_container_width=True)
+
+        st.plotly_chart(charts.rate_line_chart(df=df_diff, y=option), use_container_width=True)
+
+        st.plotly_chart(charts.histogram_chart(df=df_diff, x=option), use_container_width=True)
+
+        st.plotly_chart(charts.cumulative_dist_chart(df=df_diff, x=option), use_container_width=True)
